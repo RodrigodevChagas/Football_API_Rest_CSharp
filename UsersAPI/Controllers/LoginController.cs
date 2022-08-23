ce9ca91e6ed744928ee958292cf9cdcfbabbdf05
Rodrigo <dyggo.ag22@gmail.com>
2022-08-23 13:08:35 -0300

    criando controller de login

---
+++ UsersAPI/Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsersAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
